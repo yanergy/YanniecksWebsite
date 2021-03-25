74db05f069afeb395e8e40c7151a1376c46ae9a5
yanergy <56261297+yanergy@users.noreply.github.com>
2021-03-25 15:10:13 +0100

    updates

---
+++ footer.js
@@ -0,0 +1,9 @@
+var html = 
+'<p> Copyright &copy;\
+    <script type="text/javascript">\
+        var theDate=new Date(); document.write(theDate.getFullYear())\
+    </script>\
+      | Yanniecks Website | Contact: 306410@edu.ubboemmius.nl <a href="https://editor.p5js.org/yannieckblaauw@gmail.com/present/yelxqU_FB" style="color:transparent" target="_blank">-</a>\
+</p>'
+
+document.getElementById('footer').innerHTML = html;
---
+++ nav3.js
@@ -0,0 +1,41 @@
+var html = '<h1> Yanniecks Website </h1>\
+<nav>\
+    <ul>\
+        <li> <a href="../index.html"><b> Home </b></a> </li>\
+        <li>\
+            <div class="dropdown">\
+                <button class ="dropbtn"><b> Mijn Hobbies </b></button>\
+                <div class="dropdown-content">\
+                    <a href="../HobbyTekenen.html"><b> Tekenen </b></a>\
+                    <a href="../HobbyProgrammeren.html"><b> Programmeren </b></a>\
+                </div>\
+            </div>\
+        </li>\
+        <li> <a href="../OverMij.html"><b> Over mij </b></a></li>\
+        <!--<li> <a href="flexbox.html"><b> Flexbox </b></a></li>-->\
+        <li> <a href="../dobbelen.html"><b> Dobbelen </b></a></li>\
+        <li>\
+            <div class="dropdown">\
+                <button class ="dropbtn"><b> Havo 4 </b></button>\
+                <div class="dropdown-content">\
+                    <a href="../Havo4/Per1.html"><b> Periode 1 </b></a>\
+                    <a href="../Havo4/Per2.html"><b> Periode 2 </b></a>\
+                    <a href="../Havo4/Per3.html"><b> Periode 3 </b></a>\
+                    <a href="../Havo4/Per4.html"><b> Periode 4 </b></a>\
+                </div>\
+            </div>\
+        </li>\
+        <li>\
+            <div class="dropdown">\
+                <button class ="dropbtn"><b> Havo 5 </b></button>\
+                <div class="dropdown-content">\
+                    <a href="Per1.html"><b> Periode 1 </b></a>\
+                    <a href="Per2.html"><b> Periode 2 </b></a>\
+                    <a href="Per3.html"><b> Periode 3 </b></a>\
+                </div>\
+            </div>\
+        </li>\
+    </ul>\
+</nav>'
+
+document.getElementById('header3').innerHTML = html;
